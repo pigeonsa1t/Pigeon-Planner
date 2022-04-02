fec9ae33408dbd9eff7e55884b47901f0adcc423
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-02 12:30:30 -0400

    setting up files/organizing and psuedo code for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//** PIGEON - PLANNER **//
+
+// IMPORTS: //
+    // *like "moment", NODE libraries, etc.
+        // git@github.com:pigeonsa1t/Pigeon-Planner.git
+        // https://momentjs.com/
+
+// GLOBAL VARIABLES: //
+    // *usually, global variables are for big arrays full of data
+        // var something = function (nothing) {
+        //     console.log("sup homie")
+        // };
+        // var somethingElse = theThing + "yes/no";
+        // var theThing = 1 + 2;
+        // var anotherThing = thatThing();
+        // var thisThing = something;
+
+// FUNCTIONS: //
+    // *these will go in the order that they flow down the page.
+
+// EXPORTS: //
+    // *like reusing code on another sheet.

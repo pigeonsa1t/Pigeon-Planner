ea2b1fd2f9208ac82338243cd4d55119860b6e79
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-03 02:14:41 -0400

    added moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 //** PIGEON - PLANNER **//
 
 // IMPORTS: //
-    // *like "moment", NODE libraries, etc.
         // git@github.com:pigeonsa1t/Pigeon-Planner.git
         // https://momentjs.com/
 
+// **!!! how do i use the moment.js?? i need to import it here...
+var help = iNeedHelp();
+
 // GLOBAL VARIABLES: //
     // *usually, global variables are for big arrays full of data
-        // var something = function (nothing) {
-        //     console.log("sup homie")
-        // };
-        // var somethingElse = theThing + "yes/no";
-        // var theThing = 1 + 2;
-        // var anotherThing = thatThing();
-        // var thisThing = something;
+
+// THIS WILL NEED TO BE A VARIABLE DEFINING
+var currentDay = {
+
+};
+
+var prevDay = {
+    
+};
+
+var nextDay = {
+
+};
+
+var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
 
 // FUNCTIONS: //
     // *these will go in the order that they flow down the page.

b9d41a318261a1229b14d59dd96597c517c9b190
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-06 16:01:00 -0400

    added save capabilities with tutor

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,10 @@ var elementHour = parseInt(splitId[1]);
     }
     });
 };
-
 // =========================================================//
 // FUNCTIONS: //
 
+// ON CLICK, SAVE BUTTON WILL STORE THE TASK AND HOUR IT SHOULD BE COMPLETED IN LOCAL STORAGE
 $(".saveBtn").on("click", function() {
     var activity = $(this).siblings(".description").val()
     //console.log(activity)
@@ -46,8 +46,22 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(hour, activity)
 });
 
+// CHECK FOR TIME: 
 checkHour();
 
+// INSTEAD OF UPDATING VARIABLES ACCROSS THE PROJECT, AND AFTER WORKING W TUTOR...
+// I WILL JUST WRITE A SHORT FUNCTION HERE FOR EACH HOUR: 
+
+// LOCAL STORAGE FOR EACH ROW:
+$("#hour8 .description").val(localStorage.getItem(8));
 $("#hour9 .description").val(localStorage.getItem(9));
+$("#hour10 .description").val(localStorage.getItem(10));
+$("#hour11 .description").val(localStorage.getItem(11));
+$("#hour12 .description").val(localStorage.getItem(12));
+$("#hour13 .description").val(localStorage.getItem(13));
+$("#hour14 .description").val(localStorage.getItem(14));
+$("#hour15 .description").val(localStorage.getItem(15));
+$("#hour16 .description").val(localStorage.getItem(16));
+$("#hour17 .description").val(localStorage.getItem(17));
 
 // =========================================================//
